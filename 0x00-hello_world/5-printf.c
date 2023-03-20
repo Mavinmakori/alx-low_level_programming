5c965f8a1c29fa401e21f092e90cbb61094bc290
Mavin makori <mrvnmakori@gmail.com>
2023-03-19 18:29:56 -0700

    frist comm

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints using printf functon
+ * Return: 0(sccess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
